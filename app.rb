0177143280c966fe1c0deec6addc7d79d524175e
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2023-10-15 10:21:32 -0400

    feature: rate limits and cors added

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+require 'rack/cors'
+require 'rack/throttle'
 require_relative 'routes/alliances_routes'
+
+use Rack::Cors do
+    allow do
+      origins '*'
+      resource '*', headers: :any, methods: [:get]
+    end
+  end
+
+use Rack::Throttle::Interval,    :min => 1.0  
+use Rack::Throttle::Hourly,      :max => 100  
+use Rack::Throttle::Daily,       :max => 1000 
 
 configure do
   set :server, :webrick
